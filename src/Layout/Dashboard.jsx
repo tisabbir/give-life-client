217d27a446b6c39b8c239396c88cd007c2a157a6
TI Sabbir <tisabbir0@gmail.com>
2024-06-09 13:19:25 +0600

    clicking on the edit button the profile become editable

--- src/Layout/Dashboard.jsx
+++ src/Layout/Dashboard.jsx
@@ -3,8 +3,8 @@ import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
   return (
-    <div className="flex">
-      <div className="w-64 pt-24 pl-6 bg-[#9B111E] text-white min-h-screen">
+    <div className="flex flex-col md:flex-row">
+      <div className="w-full md:w-64 pt-24 pl-6 bg-[#9B111E] pb-4 text-white md:min-h-screen">
         <NavLink to={'/dashboard/profile'} className='flex items-center gap-1 '> <FaPerson /> Profile</NavLink>
       </div>
       <div className="pt-24 w-full">
--- src/Pages/Dashboard/Profile/Profile.jsx
+++ src/Pages/Dashboard/Profile/Profile.jsx
@@ -2,54 +2,57 @@ import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
 import useAxiosSecure from "../../../Hooks/useAxiosSecure";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
-
+import { FaPenToSquare } from "react-icons/fa6";
+import { useState } from "react";
 
 const Profile = () => {
+  const axiosSecure = useAxiosSecure();
+  const { user } = useAuth();
+  const [editable, setEditable] = useState(false);
 
-    const axiosSecure= useAxiosSecure();
-    const {user} = useAuth();
+  const { data: userInfo = {} } = useQuery({
+    queryKey: ["user"],
+    queryFn: async () => {
+      const res = await axiosSecure.get(`/users/${user.email}`);
+      return res.data;
+    },
+  });
 
-    const {data : userInfo = {}} = useQuery({
-        queryKey: ['user'],
-        queryFn : async () => {
-            const res = await axiosSecure.get(`/users/${user.email}`)
-            return res.data;
-        }
-    })
+  const { name, email, avatarUrl, district, upozila, blood } = userInfo;
 
-
-    const {
-        name,
-        email,
-        avatarUrl,
-        district,
-        upozila,
-        blood,
-      } = userInfo;
-
-
+  const handleEdit = () => {
+    setEditable(true);
+  };
 
   return (
-    <div className=" my-4">
-        <SectionTitle Heading={'User Profile'} subHeading={name} />
-      <div className="card w-96 bg-base-100 shadow-xl mx-auto">
-        <figure className="px-10 pt-10">
-          <img
-            src={avatarUrl}
-            alt="Shoes"
-            className="rounded-full"
-          />
-        </figure>
-        <div className="card-body items-center text-center">
-          <h2 className="card-title text-[#9B111E]">{name}</h2>
-         <div>
-            <p>Email : {email}</p>
-            <p>District : {district}</p>
-            <p>Upozila : {upozila}</p>
-            <p>Blood Group : {blood}</p>
-         </div>
+    <div className="mb-12">
+      <SectionTitle Heading={"User Profile"} subHeading={name} />
+
+      {editable ? 
+      (
+        <div> editable </div>
+      ) : (
+        <div className="card w-96 bg-base-100 shadow-xl mx-auto mt-4">
+          <div className="flex  justify-end">
+            <FaPenToSquare
+              onClick={handleEdit}
+              className="text-2xl text-[#9B111E] mr-4 hover:text-gray-500"
+            />
+          </div>
+          <figure className="px-10 pt-10">
+            <img src={avatarUrl} alt="Shoes" className="rounded-full" />
+          </figure>
+          <div className="card-body items-center text-center">
+            <h2 className="card-title text-[#9B111E]">{name}</h2>
+            <div>
+              <p>Email : {email}</p>
+              <p>District : {district}</p>
+              <p>Upozila : {upozila}</p>
+              <p>Blood Group : {blood}</p>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
