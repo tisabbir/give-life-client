6279e2317c760d06700c49d6a26267b2458f6ede
TI Sabbir <tisabbir0@gmail.com>
2024-06-10 11:00:42 +0600

    update functionality added

--- src/Pages/Dashboard/UpdateRequest/UpdateRequest.jsx
+++ src/Pages/Dashboard/UpdateRequest/UpdateRequest.jsx
@@ -4,22 +4,59 @@ import { useState } from "react";
 import DatePicker from "react-datepicker";
 
 import "react-datepicker/dist/react-datepicker.css";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const UpdateRequest = () => {
   const request = useLoaderData();
   const [startDate, setStartDate] = useState(new Date());
   const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
 
-  const {
-    recipientName,
-    recipientDistrict,
-    recipientUpozila,
-    hospital,
-    address,
-    requestMessage,
-  } = request;
-  const handleSubmit = () => {
-    //codes here
+
+ 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const recipientName = form.name.value;
+    const recipientDistrict = form.district.value;
+    const recipientUpozila = form.upozila.value;
+    const hospital = form.hospital.value;
+    const address = form.address.value;
+    const donationDateAndTime = startDate;
+    const requestMessage = form.message.value;
+    const donationStatus = request.donationStatus;
+
+
+    const updatedDonationRequest = {
+        donationStatus,
+        recipientName,
+        recipientDistrict,
+        recipientUpozila,
+        hospital,
+        address,
+        donationDateAndTime,
+        requestMessage,
+    }
+
+    axiosSecure.patch(`/donationRequests/${request._id}`, updatedDonationRequest)
+    .then(res => {
+        console.log(res.data);
+        if(res.data.modifiedCount>0){
+            Swal.fire({
+                position: "top-end",
+                icon: "success",
+                title: "Your donation request has been updated",
+                showConfirmButton: false,
+                timer: 1500
+              });
+        }
+    })
+    .catch(err => {
+        console.log(err);
+    })
+
+
   };
   return (
     <div>
@@ -62,7 +99,7 @@ const UpdateRequest = () => {
                 </label>
                 <input
                   type="text"
-                  defaultValue={recipientName}
+                  defaultValue={request.recipientName}
                   name="name"
                   className="input input-bordered"
                   required
@@ -75,7 +112,7 @@ const UpdateRequest = () => {
                 </label>
                 <input
                   type="text"
-                 defaultValue={recipientDistrict}
+                 defaultValue={request.recipientDistrict}
                   name="district"
                   className="input input-bordered"
                   required
@@ -88,7 +125,7 @@ const UpdateRequest = () => {
                 </label>
                 <input
                   type="text"
-                 defaultValue={recipientUpozila}
+                 defaultValue={request.recipientUpozila}
                   name="upozila"
                   className="input input-bordered"
                   required
@@ -101,7 +138,7 @@ const UpdateRequest = () => {
                 </label>
                 <input
                   type="text"
-                  defaultValue={hospital}
+                  defaultValue={request.hospital}
                   name="hospital"
                   className="input input-bordered"
                   required
@@ -114,7 +151,7 @@ const UpdateRequest = () => {
                 </label>
                 <input
                   type="text"
-                 defaultValue={address}
+                 defaultValue={request.address}
                   name="address"
                   className="input input-bordered"
                 />
@@ -139,7 +176,7 @@ const UpdateRequest = () => {
                 </label>
                 <textarea
                   type="text"
-                  defaultValue={requestMessage}
+                  defaultValue={request.requestMessage}
                   name="message"
                   className="textarea textarea-bordered"
                 />
