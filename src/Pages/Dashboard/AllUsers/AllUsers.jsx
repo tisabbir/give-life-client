7f5e6600053b4dc7d96a98e1a31600751cbcefee
TI Sabbir <tisabbir0@gmail.com>
2024-06-12 06:31:18 +0600

    All users UI Design Done, Alhamdulillah

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -1,11 +1,86 @@
-
+import { useQuery } from "@tanstack/react-query";
+import useAxiosSecure from "../../../Hooks/useAxiosSecure";
+import { FaBars } from "react-icons/fa6";
 
 const AllUsers = () => {
-    return (
-        <div>
-            All Users are here
-        </div>
-    );
+  const axiosSecure = useAxiosSecure();
+
+  const { data: users = [] } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/users");
+      return res.data;
+    },
+  });
+
+  console.log(users);
+
+  return (
+    <div>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Avatar</th>
+              <th>Email</th>
+              <th>Name</th>
+              <th>Role</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, index) => (
+              <tr key={index}>
+                <th>{index + 1}</th>
+                <td>
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img
+                        src={user.avatarUrl}
+                        alt="Avatar Tailwind CSS Component"
+                      />
+                    </div>
+                  </div>
+                </td>
+                <td>{user.email}</td>
+                <td>{user.name}</td>
+                <td>{user.role}</td>
+                <td>{user.status}</td>
+                <th>
+                  <div className="dropdown dropdown-hover dropdown-bottom dropdown-end">
+                    <div tabIndex={0} role="button" className="btn m-1">
+                      <FaBars />
+                    </div>
+                    <ul
+                      tabIndex={0}
+                      className="dropdown-content z-[10] menu p-2 shadow bg-base-100 rounded-box w-52"
+                    >
+                      <li>
+                        {user.status === 'active' ? <button>Block</button> : <button>Unblock</button>}
+                      </li>
+                      <li>
+                        {
+                            user.role === "donor" ? <button>Make Volunteer</button> : ''
+                        }
+                      </li>
+                      <li>
+                        {
+                            user.role === "donor" || user.status === 'volunteer' ? <button>Make Admin</button> : ''
+                        }
+                      </li>
+                    </ul>
+                  </div>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 };
 
 export default AllUsers;
