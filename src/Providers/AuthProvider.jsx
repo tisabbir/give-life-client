ff651391765e8e442b1797d464626d64557af952
TI Sabbir <tisabbir0@gmail.com>
2024-06-12 16:24:11 +0600

    now the jwt token is created from backend and send to frontend and stored in local storage

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -7,11 +7,13 @@ import {
   signOut,
   updateProfile,
 } from "firebase/auth";
+import useAxiosPublic from "../Hooks/useAxiosPublic";
 
 export const AuthContext = createContext(auth);
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const axiosPublic = useAxiosPublic();
 
   const createUser = (email, password) => {
     setLoading(true);
@@ -36,14 +38,30 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+
       setUser(currentUser);
+      if(currentUser){
+        //code goes here
+        const userInfo = {email: currentUser.email}
+        axiosPublic.post('/jwt', userInfo )
+        .then(res => {
+          console.log(res.data.token);
+          if(res.data.token){
+            localStorage.setItem('access-token', res.data.token);
+          }
+        }).catch(err => {
+          console.log(err);
+        })
+      } else {
+        localStorage.removeItem('access-token')
+      }
       setLoading(false);
     });
 
     return () => {
       unsubscribe();
     };
-  }, []);
+  }, [axiosPublic]);
 
   const authInfo = {
     user,
